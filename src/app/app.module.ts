48ab0e7997b9ef62391da3805eb56d6a7bf37123
juanxo13597 <juanxo97master@gmail.com>
2022-07-06 22:57:46 +0200

    tamaño de paginacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import * as $ from 'jquery';
 import { HeaderComponent } from './modules/header/header.component';
 import { HeaderModule } from './modules/header/header.module';
 import { HomeModule } from './modules/home/home.module';
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -11,21 +11,19 @@ export class HomeComponent implements OnInit {
   public pokemons: Array<pokemonModel>;
   public loading: boolean;
   public initPag: number;
-  public limit: number;
   public tamPag: number;
   constructor(private _service: HomeService) {
     this.loading = false;
     this.pokemons = [];
     this.initPag = 0;
-    this.limit = 5;
     this.tamPag = 5;
   }
 
   ngOnInit(): void {
-    this.getPokemons(this.initPag, this.limit);
+    this.getPokemons(this.initPag, this.tamPag);
   }
 
-  private getPokemons(init: number, limit: number = this.limit): void {
+  private getPokemons(init: number, limit: number = this.tamPag): void {
     this._service.getPokemons(init, limit).then((data) => {
       this.pokemons = data;
       this.loading = true;
@@ -39,6 +37,16 @@ export class HomeComponent implements OnInit {
   }
 
   public pagAnterior() {
-    console.log('anterior');
+    this.loading = false;
+    if (this.initPag - this.tamPag >= 0) {
+      this.initPag -= this.tamPag;
+      this.getPokemons(this.initPag);
+    }
+  }
+
+  public selectPage() {
+    let limit = Number($('#selectPage option:selected').val());
+    this.tamPag = limit;
+    this.getPokemons(this.initPag, limit);
   }
 }
